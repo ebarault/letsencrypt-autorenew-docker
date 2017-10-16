6e3abfcc2d3d6e5a3f0a5d132b6e455f898a153d
ebarault <eric.barault@gmail.com>
2017-10-16 22:20:08 +0200

    adding -wait for healthy- loop

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
+echo "Waiting for $HEALTH_CHECK_URL to be online"
 
-# one-time execution at container start
+until [ $(curl -s -L --head --fail -o /dev/null -w '%{http_code}\n' --connect-timeout 3 --max-time 5 $HEALTH_CHECK_URL) -eq 200 ]; do
+  printf '.'
+  sleep 5
+  # -s = Silent cURL's output
+  # -L = Follow redirects
+  # -w = Custom output format
+  # -o = Redirects the HTML output to /dev/null
+done
+
+# one-time execution at container start once host's health check is ok
 /scripts/run_certbot.sh
 
 # scheduling periodic executions
